c53a697dcf353cf2ce45489d1dc3e2d4c6a02d4d
Newstar <newstar@Newstarui-MacBook-Pro.local>
2017-08-06 22:03:15 +0900

    1:1 inquire & Reserve UI

--- SmartStay/app/src/main/java/com/wap/smartstay/Fragment/HomeFragment.java
+++ SmartStay/app/src/main/java/com/wap/smartstay/Fragment/HomeFragment.java
@@ -1,12 +1,17 @@
 package com.wap.smartstay.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 
+import com.wap.smartstay.Manual;
 import com.wap.smartstay.R;
+import com.wap.smartstay.Reserve;
 
 public class HomeFragment extends Fragment {
     @Override
@@ -20,7 +25,35 @@ public class HomeFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.home_fragment, container, false);
 
-        // TODO: 2017. 7. 31. 홈에서 동작할 버튼 여기다가 구현
+        Button reserveBtn1 = (Button) view.findViewById(R.id.reserveBtn1);
+        reserveBtn1.setOnClickListener(
+                new Button.OnClickListener() {
+                    public void onClick(View v) {
+                        Intent manual = new Intent(getContext(), Reserve.class);
+                        startActivity(manual);
+                    }
+                }
+        );
+
+        Button reserveBtn2 = (Button) view.findViewById(R.id.reserveBtn2);
+        reserveBtn2.setOnClickListener(
+                new Button.OnClickListener() {
+                    public void onClick(View v) {
+                        Intent manual = new Intent(getContext(), Reserve.class);
+                        startActivity(manual);
+                    }
+                }
+        );
+
+        Button reserveBtn3 = (Button) view.findViewById(R.id.reserveBtn3);
+        reserveBtn3.setOnClickListener(
+                new Button.OnClickListener() {
+                    public void onClick(View v) {
+                        Intent manual = new Intent(getContext(), Reserve.class);
+                        startActivity(manual);
+                    }
+                }
+        );
 
         return view;
     }
