e14eac9d3869cfdc615ef3532281b94271776e01
KDS <sexyboyds@naver.com>
2017-08-14 14:45:59 +0900

    AlmostDS

--- SmartStay/app/src/main/java/com/wap/smartstay/Main.java
+++ SmartStay/app/src/main/java/com/wap/smartstay/Main.java
@@ -8,10 +8,12 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.google.firebase.iid.FirebaseInstanceId;
 import com.wap.smartstay.Fragment.HomeFragment;
 import com.wap.smartstay.Fragment.MypageFragment;
 import com.wap.smartstay.Fragment.SmartkeyFragment;
@@ -29,6 +31,9 @@ public class Main extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
+
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d("KDS", "Refreshed token: " + refreshedToken);
 
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_main);
