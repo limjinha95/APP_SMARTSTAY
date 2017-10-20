ea9eeac516dd0a2982bc5023786c6b825eb1dc91
n2wstar <n2wstar@gmail.com>
2017-10-21 00:36:11 +0900

    android unite

--- SmartStay/app/src/main/java/com/wap/smartstay/Fragment/SmartkeyFragment.java
+++ SmartStay/app/src/main/java/com/wap/smartstay/Fragment/SmartkeyFragment.java
@@ -1,6 +1,8 @@
 package com.wap.smartstay.Fragment;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.TypedArray;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
 import android.os.Bundle;
@@ -8,6 +10,8 @@ import android.os.StrictMode;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AlertDialog;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -24,8 +28,16 @@ import com.wap.smartstay.R;
 import com.wap.smartstay.SmartKeyCallingList;
 import com.wap.smartstay.SmartkeyPopupList;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SmartkeyFragment extends Fragment {
-    public static String phoneNumber;
+    private RecyclerView recyclerView;
+    private String[] hotelNames;
+    private String[] roomNames;
+    private TypedArray roomPics;
+    private List<SmartkeyList> SmartkeyList;
+    private static Context context = null;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -35,22 +47,29 @@ public class SmartkeyFragment extends Fragment {
             StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
             StrictMode.setThreadPolicy(policy);
         }
-
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.smartkey_fragment, container, false);
 
-        ImageButton manualBtn = (ImageButton) view.findViewById(R.id.manualBtn);
-        manualBtn.setOnClickListener(
-                new Button.OnClickListener() {
-                    public void onClick(View v) {
-                        Intent manual = new Intent(getContext(), Manual.class);
-                        startActivity(manual);
-                    }
-                }
-        );
+        context=getActivity();
+
+        hotelNames = getResources().getStringArray(R.array.hotelNames);
+        roomNames = getResources().getStringArray(R.array.roomNames);
+        roomPics = getResources().obtainTypedArray(R.array.roomPics);
+
+        SmartkeyList = new ArrayList<SmartkeyList>();
+        for(int i=0; i<roomNames.length; i++){
+            SmartkeyList smartkeyList = new SmartkeyList(hotelNames[i], roomNames[i], roomPics.getResourceId(i,-1));
+            SmartkeyList.add(smartkeyList);
+        }
+
+        recyclerView = (RecyclerView)view.findViewById(R.id.recyclerview);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(SmartkeyFragment.context);
+        recyclerView.setLayoutManager(layoutManager);
+        SmartkeyFragmentAdapter adapter = new SmartkeyFragmentAdapter(SmartkeyList,SmartkeyFragment.context);
+        recyclerView.setAdapter(adapter);
 
         LinearLayout manualBackground = (LinearLayout) view.findViewById(R.id.manualBackground);
         manualBackground.setOnClickListener(new Button.OnClickListener() {
@@ -61,35 +80,30 @@ public class SmartkeyFragment extends Fragment {
             }
         });
 
-        ImageButton smartkeyBtn = (ImageButton) view.findViewById(R.id.smartkeyBtn);
-        smartkeyBtn.setOnClickListener(
-                new Button.OnClickListener() {
-                    public void onClick(View v) {
-                        Intent smartkey = new Intent(getContext(), SmartkeyPopupList.class);
-                        startActivity(smartkey);
-                    }
-                }
-        );
-
-        ImageButton addgroupBtn = (ImageButton) view.findViewById(R.id.addgroupBtn);
-        addgroupBtn.setOnClickListener(
-                new Button.OnClickListener() {
-                    public void onClick(View v) {
-                        Intent addgroup = new Intent(getContext(), AddGroup.class);
-                        startActivity(addgroup);
-                    }
-                }
-        );
-
-        ImageButton callBtn = (ImageButton) view.findViewById(R.id.callBtn);
-        callBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent smartkey = new Intent(getContext(), SmartKeyCallingList.class);
-                startActivity(smartkey);
-
-            }
-        });
+//        ImageButton smartkeyBtn = (ImageButton) view.findViewById(R.id.smartkeyBtn);
+//        smartkeyBtn.setOnClickListener(
+//                new Button.OnClickListener() {
+//                    public void onClick(View v) {
+//                    }
+//                }
+//        );
+//
+//        ImageButton addgroupBtn = (ImageButton) view.findViewById(R.id.addgroupBtn);
+//        addgroupBtn.setOnClickListener(
+//                new Button.OnClickListener() {
+//                    public void onClick(View v) {
+//                        Intent addgroup = new Intent(getContext(), AddGroup.class);
+//                        startActivity(addgroup);
+//                    }
+//                }
+//        );
+//
+//        ImageButton callBtn = (ImageButton) view.findViewById(R.id.callBtn);
+//        callBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//            }
+//        });
         return view;
     }
 
